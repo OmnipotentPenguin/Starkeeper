315c356c451aa838000c1111be4476a92dfde7c0
OmnipotentPenguin <adammartin@hotmail.co.uk>
2019-12-17 17:34:29 +0000

    Rejoined Article and articletag columns

--- src/main/java/main/repository/entities/Article.java
+++ src/main/java/main/repository/entities/Article.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
@@ -17,7 +16,7 @@ import javax.persistence.Table;
 public class Article {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue
 	private long id;
 	private String name;	
 	private String description;	
--- src/main/java/main/repository/entities/ArticleTag.java
+++ src/main/java/main/repository/entities/ArticleTag.java
@@ -1,18 +1,19 @@
 package main.repository.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table
 public class ArticleTag {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@Column(name = "tag_name")
 	private String name;
 	
 	public ArticleTag() {
